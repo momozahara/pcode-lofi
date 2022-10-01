59d2ce37bbe1aa3ccd2b9a353223e824a7432f27
Pream Pinbut <momozahara@hotmail.com>
2022-10-01 22:14:31 +0700

    update components

---
+++ src/components/channel-list.tsx
@@ -0,0 +1,34 @@
+export default [
+  {
+    name: "bootleg smoke",
+    key: "bLlloaA4b4g",
+  },
+  {
+    name: "pig smoke",
+    key: "j4sJkuOPUP8",
+  },
+  {
+    name: "chillhop raccoon",
+    key: "5yx6BWlEVcY",
+  },
+  {
+    name: "chillhop relaxing raccoon",
+    key: "7NOSDKb0HlU",
+  },
+  {
+    name: "college girl",
+    key: "MCkTebktHVc",
+  },
+  {
+    name: "college guy",
+    key: "kgx4WGK0oNU",
+  },
+  {
+    name: "college guy piano",
+    key: "tfBVp0Zi2iE",
+  },
+  {
+    name: "coffee shop",
+    key: "-5KAN9_CzSA",
+  },
+];
