e3fba52e6c3c544a2998fa8c34a86113364787b6
Pream Pinbut <momozahara@hotmail.com>
2022-11-13 11:27:05 +0700

    add hostname env

--- components/channel.ts
+++ components/channel.ts
@@ -1,6 +1,5 @@
-// prepare for prisma in the future
 export const getChannel = async () => {
-  const response = await fetch("http://localhost:3000/api/channel", {
+  const response = await fetch(`${process.env.HOSTNAME}/api/channel`, {
     cache: "no-cache",
   });
   const data = await response.json();
