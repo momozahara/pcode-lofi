2f38080eb4f44ba12da2d13a467538ca896b5ab5
Pream Pinbut <momozahara@hotmail.com>
2022-11-13 12:32:13 +0700

    peepoo

--- components/channel.ts
+++ components/channel.ts
@@ -1,10 +1,5 @@
+import { prisma } from "prisma/client";
+
 export const getChannel = async () => {
-  const response = await fetch(
-    `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/channel`,
-    {
-      cache: "no-cache",
-    }
-  );
-  const data = await response.json();
-  return data;
+  return await prisma.channel.findMany();
 };
