34b5d98d6fcc1dca2bb08d6751fa56da6c2714c8
Pream Pinbut <momozahara@hotmail.com>
2022-11-29 01:50:19 +0700

    use pages

--- app/page.tsx
+++
@@ -1,7 +0,0 @@
-import { getChannel } from "components/channel";
-import Application from "components/application";
-
-export default async function Page() {
-  const channelList = await getChannel();
-  return <Application channelList={channelList} />;
-}
