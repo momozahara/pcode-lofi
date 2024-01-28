5f6d6946ffcf173d8f9df8cdb9aca43dcdb2fbb6
Pream Pinbut <momozahara@hotmail.com>
2024-01-28 21:13:55 +0700

    use built-in next font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 
-import { Montserrat } from "@next/font/google";
+import { Montserrat } from "next/font/google";
 
 const montserrat = Montserrat({
   subsets: ["latin"],
