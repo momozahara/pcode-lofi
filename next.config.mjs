f16eac5f97b598e999af3b367530cfd93eaa45a2
Pream Pinbut <momozahara@hotmail.com>
2024-08-30 02:36:48 +0700

    chore: redirect to homepage if not found instead of rewrite

--- next.config.mjs
+++ next.config.mjs
@@ -3,14 +3,6 @@ const nextConfig = {
   reactStrictMode: false,
   swcMinify: true,
   output: "standalone",
-  rewrites() {
-    return [
-      {
-        source: "/:path*",
-        destination: "/",
-      },
-    ];
-  },
 };
 
 export default nextConfig;
